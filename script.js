9401c6d48e6039018612c72131fc5779cd3cdda1
krishgon <krishgondia@gmail.com>
2021-06-06 21:49:34 +0530

    add the ability to print numbers in ascending order on the ticket

--- script.js
+++ script.js
@@ -1,14 +1,45 @@
+var generatedNumbers = [];
+
+var ticketNumbers = [];
+
+
 function goToGenerator() {
     window.location.href = "generator.html"
 }
 
 function genNewTicket() {
     var ticketCells = document.getElementsByClassName("closed");
+
+    while(ticketNumbers.length<43){
+        var numberOfBox = getUniqueNumber().toString();
+        ticketNumbers.push(numberOfBox);
+    }
+
+    console.log("next will be the original value");
+    ticketNumbers.sort()
+    console.log(ticketNumbers)
+
     for(var i = 0, len = ticketCells.length; i < len; i++){
-        var cellNumber = ticketCells[i].getElementsByTagName("span")[0];
-        cellNumber.innerHTML = generateHousieNumber();
+        var numberArea = ticketCells[i].getElementsByTagName("span")[0];
+        numberArea.innerHTML = ticketNumbers[i];
     }
 }
+
+function getUniqueNumber(){
+    var number = generateHousieNumber();
+    
+    if(generatedNumbers.length == 0){
+        generatedNumbers.push(number);
+    }else{
+        while(generatedNumbers.includes(number)==true){
+            number = generateHousieNumber();
+        }
+    }
+    // console.log();
+    return number;
+    // console.log(generatedNumbers.includes(number));
+}
+
 
 function generateHousieNumber() {
     var x;
@@ -16,6 +47,8 @@ function generateHousieNumber() {
     while ((x > 90) || (x == 0)) {
         x = getRandNum();
     }
+
+    
 
     return x;
 }
